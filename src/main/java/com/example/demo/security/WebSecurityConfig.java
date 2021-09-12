e71c252c484bd144dcdef4708b0e0c8ad870461a
kfiddle <kenjfiddle@gmail.com>
2021-09-12 11:52:38 -0400

    webSecurtityConfig disabling csrf for now

--- src/main/java/com/example/demo/security/WebSecurityConfig.java
+++ src/main/java/com/example/demo/security/WebSecurityConfig.java
@@ -51,7 +51,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.csrf().disable().authorizeRequests()
                 .anyRequest().permitAll();
 //                .and()
 //                .formLogin()
